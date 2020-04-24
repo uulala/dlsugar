00d628e98d8a2bdedac0d75a3a2a8b91831f73d4
unamelcs <18059451066@163.com>
2020-04-24 17:26:39 +0800

    fix: 优化样式

--- examples/pages/zh-CN/component.vue
+++ examples/pages/zh-CN/component.vue
@@ -16,7 +16,6 @@
     margin-top: 80px;
     transition: padding-top 0.3s;
     padding-left: 50px;
-
   }
 
   .page-component__content {
@@ -33,6 +32,51 @@
       h3 {
         margin: 55px 0 20px;
       }
+      table {
+        border-collapse: collapse;
+        width: 100%;
+        background-color: #fff;
+        font-size: 14px;
+        margin-bottom: 45px;
+        line-height: 1.5em;
+        strong {
+          font-weight: normal;
+        }
+        td,
+        th {
+          border-bottom: 1px solid #dcdfe6;
+          padding: 15px;
+          max-width: 250px;
+        }
+        th {
+          text-align: left;
+          white-space: nowrap;
+          color: #909399;
+          font-weight: normal;
+        }
+        td {
+          color: #606266;
+        }
+        th:first-child,
+        td:first-child {
+          padding-left: 10px;
+        }
+      }
+      ul:not(.timeline) {
+        margin: 10px 0;
+        padding: 0 0 0 20px;
+        font-size: 14px;
+        color: #5e6d82;
+        line-height: 2em;
+      }
+    }
+  }
+  a {
+    &.default {
+      color: #333;
+    }
+    &.active {
+      color: #409eff;
     }
   }
 }
@@ -114,7 +158,7 @@ export default {
   },
   methods: {
     menuChange (path) {
-      this.$router.push({path})
+      this.$router.push({ path })
     }
   }
 }
